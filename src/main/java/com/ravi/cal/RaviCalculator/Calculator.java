ac1bc9b24483b441745ffe54e24a160673d7310d
IshanSharmaDevops <69542145+IshanSharmaDevops@users.noreply.github.com>
2020-11-01 00:42:57 +0530

    Update Calculator.java

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -52,6 +52,7 @@ public class Calculator  {
 		long second = sc.nextLong();
 		cc.setSecond(second);
 		System.out.println("Hi");
+		System.out.println("Hello");
 		System.out.println("The sum of two numbers is: "+cc.addFucn(first, second));
 		System.out.println("The difference of two numbers is: "+cc.subFucn(first, second));
 		System.out.println("The multiples of two numbers is: "+cc.mulFucn(first, second));
