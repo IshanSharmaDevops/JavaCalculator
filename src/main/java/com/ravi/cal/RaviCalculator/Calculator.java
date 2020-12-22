bd96df209cf60e72d512d6d7899cedf36c8fa74c
IshanSharmaDevops <69542145+IshanSharmaDevops@users.noreply.github.com>
2020-12-22 19:29:42 +0530

    Update Calculator.java

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -54,6 +54,7 @@ public class Calculator  {
 		System.out.println("Hi");
 		System.out.println("Hello");
 		System.out.println("Test Webhook");
+		System.out.println("Test Webhook1");
 		System.out.println("The sum of two numbers is: "+cc.addFucn(first, second));
 		System.out.println("The difference of two numbers is: "+cc.subFucn(first, second));
 		System.out.println("The multiples of two numbers is: "+cc.mulFucn(first, second));
